3585c83a044080f68039f583f2ada1a9d5b628fa
Bob <bob_w@qq.com>
2022-04-27 08:54:47 +0800

    learn jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,33 +3,35 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      foo: {
-        options: {
-          banner: '/*! <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        },
-        src: 'src/<%= pkg.version %>.js',
-        dest: 'build/<%= pkg.version %>.min.js'
+    jshint: {
+      options: {
+        reporterOutput: ''
       },
+      foo: {
+        src: ['src/**']
+      },
+    },
+    uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      buildp: {
+      build: {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       },
-    }
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 
   // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function () {
-    grunt.log.write('Logging some stuff...').ok();
-  });
+  // grunt.registerTask('default', 'Log some stuff.', function () {
+  //   grunt.log.write('Logging some stuff...').ok();
+  // });
 
 };
